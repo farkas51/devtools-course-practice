22312e57d4260946c539f144b9273d7ba067a745
Nikolai Groshev <kolia.avatar@gmail.com>
2022-04-17 15:22:33 +0300

    ends develop func for triangles/points

--- modules/groshev_point_in_triangle/test/test_point_in_triangle.cpp
+++ modules/groshev_point_in_triangle/test/test_point_in_triangle.cpp
@@ -13,7 +13,7 @@ TEST(PointConstructor, Constructor_With_Parameters) {
   ASSERT_NO_THROW(Point(5.0,2.0));
 }
 
-TEST(PointConstructor, Get_Coordinates_From_Default_Point) {
+TEST(PointTest, Get_Coordinates_From_Default_Point) {
   // Arrange
   Point t;
   
@@ -21,7 +21,7 @@ TEST(PointConstructor, Get_Coordinates_From_Default_Point) {
   ASSERT_TRUE((t.x == 0) && (t.y == 0));
 }
 
-TEST(PointConstructor, Get_Coordinates_From_Point) {
+TEST(PointTest, Get_Coordinates_From_Point) {
   // Arrange
   Point t(5.0,2.0);
 
@@ -46,6 +46,33 @@ TEST(TriangleConstructor, Constructor_With_Parameters) {
 }
 
 
+TEST(PointTest, Non_Equals_Point) {
+  // Arrange
+  Point a(1.0, 2.0);
+  Point b(3.0, 4.0);
+
+  // Act & Assert
+  ASSERT_FALSE(a == b);
+}
+
+TEST(PointTest, Equals_Point) {
+  // Arrange
+  Point a(1.0, 2.0);
+  Point b(1.0, 2.0);
+
+  // Act & Assert
+  ASSERT_TRUE(a == b);
+}
+
+TEST(PointTest, Equality_Operator) {
+  // Arrange
+  Point a(1.0, 2.0);
+  Point b = a;
+
+  // Act & Assert
+  ASSERT_TRUE(a == b);
+}
+
 
 TEST(TrianglesTest, Throws_when_incorrect_points) {
   // Arrange
@@ -55,5 +82,44 @@ TEST(TrianglesTest, Throws_when_incorrect_points) {
 
   // Act & Assert
   ASSERT_ANY_THROW(Triangle (a, b, c));
+}
+
+TEST(TrianglesTest, Not_Throws_when_correct_points) {
+  // Arrange
+  Point a(-3.0, 0.0);
+  Point b(3.0, 0.0);
+  Point c(0.0, 3.0);
+
+  // Act & Assert
+  ASSERT_NO_THROW(Triangle(a, b, c));
+}
+
+TEST(TrianglesTest, Get_points) {
+  // Arrange
+  Point a(-3.0, 0.0);
+  Point b(3.0, 0.0);
+  Point c(0.0, 3.0);
+  Triangle t(a, b, c);
+
+  // Act & Assert
+  ASSERT_TRUE(t.get_point_a() == a && t.get_point_b() == b &&
+              t.get_point_c() == c);
+}
+
+
+TEST(TrianglesTest, Correct_side_length) {
+  // Arrange
+  Point a(-3.0, 0.0);
+  Point b(3.0, 0.0);
+  Point c(0.0, 3.0);
+  Triangle t(a, b, c);
+
+  double length_ab = t.SideLength(a, b);
+  double length_ac = t.SideLength(a, c);
+  double length_bc = t.SideLength(b, c);
+
+
+  // Act & Assert
+  ASSERT_TRUE(length_ab == 6 && length_ac == length_bc);
 }
 
